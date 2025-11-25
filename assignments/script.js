8dda9fc40ca7108f1363163830cff2fbe7574e0d
mhd <mahmoud.shuman@outlook.com>
2025-11-25 14:11:58 +0200

    first commit

---
+++ assignments/script.js
@@ -0,0 +1,36 @@
+console.log(example);
+var example = "I'm the example" ;
+//    1  
+
+console.log(hello);
+var hello = "world"
+
+//      2
+
+var needle = "haystack";
+test();
+function test(){
+    var needle = 'magnet';
+    console.log(needle);
+}
+
+//   3
+
+var  brendan = 'super cool';
+function print(){
+    brendan = 'only okay';
+    console.log(brendan);
+
+}
+console.log(brendan);
+
+//   4
+
+var food = 'chicken';
+console.log(food);
+eat();
+function eat(){
+    food = 'half chicken';
+    console.log(food);
+    var food = 'gone'
+}
