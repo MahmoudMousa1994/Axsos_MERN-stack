d0026ba4310e666d6b1e05d6d4fb07db1f271331
mhd <mahmoud.shuman@outlook.com>
2025-11-25 16:21:58 +0200

    hoist assignment

--- assignments/script.js
+++ assignments/script.js
@@ -34,3 +34,54 @@ function eat(){
     console.log(food);
     var food = 'gone'
 }
+
+//  5
+mean();
+console.log(food);
+var mean = function(){
+    food = "chicken";
+    console.log(food);
+    var food = "fish";
+    console.log(food);
+}
+console.log(food);
+
+
+// 6
+console.log(gener);
+var gener = "disco";
+rewind();
+function rewind(){
+    gener = "rock";
+    console.log(gener);
+    var gener = "r&b";
+    console.log(gener);
+}
+console.log(gener);
+
+
+// 7
+dojo = "sanjose";
+console.log(dojo);
+learn();
+function learn(){
+    dojo = "seattle";
+    console.log(dojo);
+}
+console.log(dojo);
+
+// 8 
+console.log(makeDojo("Chicago",65));
+console.log(makeDojo("Berkeley",0));
+function makeDojo(name, students){
+    const dojo = {};
+    dojo.name = name;
+    dojo.students = students;
+    if (dojo.students > 50){
+        dojo.hiring = true;
+    }
+    else if (dojo.students<=0){
+        dojo = "closed for now"
+    }
+    return dojo;
+}
