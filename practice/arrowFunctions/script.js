7624a8c5fdf9c032dbd2232872ce163e818b281d
mhd <mahmoud.shuman@outlook.com>
2025-12-29 18:46:19 +0200

    convert to arrow

--- practice/arrowFunctions/script.js
+++ practice/arrowFunctions/script.js
@@ -37,3 +37,35 @@ const returnObjLonghand=()=>{
 
 // const returnObj=()=>{firstName:'John',lastName:'Wick'};   this dont work without parenthesis
 const returnObj=()=>({firstName:'John',lastName:'Wick'});
+
+
+// class Deck{
+//     constructor(){
+//         const suits =['Diamond','Heart','Spade','Club'];
+//         const faces =['Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King'];
+//         const deck = [];
+//         suits.forEach(function(suit){
+//             faces.forEach(function(face){
+//                 deck.push(this.creatCard(suit,face));
+//             });
+//         });
+//         this.deck= deck;
+//     }
+// }
+
+class Deck{
+    constructor(){
+        const suits =['Diamond','Heart','Spade','Club'];
+        const faces =['Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King'];
+        const deck = [];
+        suits.forEach(suit=>{
+            faces.forEach(face=>{
+                deck.push(this.createCard(suit,face));
+            });
+        });
+        this.deck = deck;  // Assign to instance property
+    }
+    createCard(suit, face){  // Define the method
+        return { suit, face };
+    }
+}
